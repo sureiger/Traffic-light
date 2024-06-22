add5106e8006c7bd64e2321a56edd19118506fc2
sureiger <sergei.sugar@yandex.ru>
2024-06-22 20:06:26 +0300

    First

---
+++ 1/1.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+from threading import Thread
+import time
+
+
+class TrafficLightApp:
+    def init(self, root):
+        self.root = root
+        self.root.title("Управление Светофорами")
+
+        # Настройка меток для отображения животных
+        self.head1 = tk.Label(
+            root, text='Овцы', font=('Helvetica', 18),)
+        self.head1.grid(row=0, column=0)
+
+        self.head2 = tk.Label(
+            root, text='Козы', font=('Helvetica', 18),)
+        self.head2.grid(row=1, column=0)
+        
+        # Настройка меток для отображения состояний светофоров
+        self.label1 = tk.Label(
+            root, text='Светофор', font=('Helvetica', 18),)
+        self.label1.grid(row=0, column=1)
+
+        self.label2 = tk.Label(
+            root, text='Светофор', font=('Helvetica', 18))
+        self.label2.grid(row=1, column=1)
+
+        # Запуск потока управления светофорами
+        self.thread = Thread(target=self.manage_lights)
+        self.thread.start()
+
+    def manage_lights(self):
+        while True:
+
+            self.label1.config(bg='green')
+            self.label2.config(bg='red')
+            self.update_gui()
+            time.sleep(5)
+
+            self.label1.config(bg='yellow')
+            self.update_gui()
+            time.sleep(3)
+
+            self.label1.config(bg='red')
+            self.label2.config(bg='green')
+            self.update_gui()
+            time.sleep(5)
+
+            self.label2.config(bg='yellow')
+            self.update_gui()
+            time.sleep(3)
+
+    def update_gui(self):
+        # Обновление GUI
+        self.root.update_idletasks()
+        self.root.update()
+
+
+# Создание окна приложения
+root = tk.Tk()
+app = TrafficLightApp(root)
+root.mainloop()
